1ca359c20b9a305b106859e644625e95060c4446
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2017-11-04 22:44:57 +0800

    Initial Commit

---
+++ ThirdLibPackage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThirdLibPackage
+//
+//  Created by mgfjx on 2017/11/4.
+//  Copyright © 2017年 mgfjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThirdLibPackage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThirdLibPackage
+//
+//  Created by mgfjx on 2017/11/4.
+//  Copyright © 2017年 mgfjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
