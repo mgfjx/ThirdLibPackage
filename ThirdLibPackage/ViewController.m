14a9063f3150c1fb6e6cdde661ed302a12db6143
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2017-11-05 21:39:58 +0800

    测试tableView、collectionView

--- ThirdLibPackage/ViewController.m
+++ ThirdLibPackage/ViewController.m
@@ -7,10 +7,9 @@
 //
 
 #import "ViewController.h"
-#import "RefreshScrollView.h"
 #import "UIScrollView+Refresh.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource>
 
 @property (nonatomic, strong) UIScrollView *scrollView ;
 
@@ -21,7 +20,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self initScrollView];
+//    [self initTableView];
+    [self initCollectionView];
     
     
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
@@ -34,23 +34,15 @@
 }
 
 - (void)setPropertys:(UIButton *)sender {
-//    sender.selected = !sender.selected;
-//    self.scrollView.canPullUp = sender.selected;;
-//    self.scrollView.canPullDown = !sender.selected;
-    
-    NSLog(@"1%@", _scrollView);
-    [_scrollView removeFromSuperview];
-    _scrollView = nil;
-    NSLog(@"2%@", _scrollView);
+    sender.selected = !sender.selected;
+    self.scrollView.canPullUp = sender.selected;
+    self.scrollView.canPullDown = !sender.selected;
 }
 
-- (void)initScrollView {
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    scrollView.backgroundColor = [UIColor randomColor];
-    _scrollView = scrollView;
-    scrollView.canPullUp = YES;
-
-    scrollView.headerRefreshBlock = ^(UIScrollView *rfScrollView) {
+- (void)setMJRefresh {
+    _scrollView.canPullUp = YES;
+    
+    _scrollView.headerRefreshBlock = ^(UIScrollView *rfScrollView) {
         dispatch_async(dispatch_get_global_queue(0, 0), ^{
             [NSThread sleepForTimeInterval:2];
             dispatch_async(dispatch_get_main_queue(), ^{
@@ -58,8 +50,9 @@
             });
         });
     };
-
-    scrollView.footerRefreshBlock = ^(UIScrollView *rfScrollView){
+    
+    _scrollView.canPullDown = YES;
+    _scrollView.footerRefreshBlock = ^(UIScrollView *rfScrollView){
         dispatch_async(dispatch_get_global_queue(0, 0), ^{
             [NSThread sleepForTimeInterval:2];
             dispatch_async(dispatch_get_main_queue(), ^{
@@ -67,8 +60,12 @@
             });
         });
     };
+}
 
-    scrollView.canPullDown = YES;
+- (void)initScrollView {
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
+    scrollView.backgroundColor = [UIColor randomColor];
+    _scrollView = scrollView;
     [self.view addSubview:scrollView];
     
     UIView *view = [[UIView alloc] initWithFrame:scrollView.bounds];
@@ -77,6 +74,79 @@
     [scrollView addSubview:view];
 
     scrollView.contentSize = CGSizeMake(scrollView.width, view.height);
+    [self setMJRefresh];
+}
+
+- (void)initTableView {
+    UITableView *table = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    table.delegate = self;
+    table.dataSource = self;
+    
+    [self.view addSubview:table];
+    _scrollView = table;
+    [self setMJRefresh];
+}
+
+- (void)initCollectionView {
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
+    layout.itemSize = CGSizeMake(50, 50);
+    layout.minimumLineSpacing = 10;
+    layout.minimumInteritemSpacing = 10;
+    
+    UICollectionView *collection = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    collection.delegate = self;
+    collection.dataSource = self;
+    collection.backgroundColor = [UIColor whiteColor];
+    [collection registerClass:UICollectionViewCell.class forCellWithReuseIdentifier:@"cell"];
+    
+    
+    
+    [self.view addSubview:collection];
+    _scrollView = collection;
+    [self setMJRefresh];
+}
+
+#pragma mark - UITableViewDelegate, UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *cellReuse = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellReuse];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellReuse];
+    }
+    
+    cell.backgroundColor = [UIColor randomColor];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+#pragma mark - UICollectionViewDelegate, UICollectionViewDataSource
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 100;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor randomColor];
+    
+    return cell;
 }
 
 @end
